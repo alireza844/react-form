d8d2080150c1a06a6ca5cad439a5e2f43322167b
Alireza Tireh <tirehAlireza916@gmail.com>
2022-08-24 16:31:03 +0430

    using react-toastify
    app created successfuly withou error in Logical part of program
    
    but styles not added yet...

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { notify } from './notify';
+
 import { validate } from './validate';
 
 const SignUp = () => {
@@ -12,11 +17,12 @@ const SignUp = () => {
         isAccepted: false,
     });
     const [errors, setErrors] = useState({})
+    const [touched, setTouched] = useState({})
 
     useEffect(() => {
         setErrors(validate(data))
         console.log(errors)
-    },[data])
+    },[data, touched])
 
     const changeHandler = (event) => {
         if(event.target.name === "isAccepted") {
@@ -26,36 +32,62 @@ const SignUp = () => {
         }
     }
 
+    const focusHandler = (event) => {
+        setTouched({...touched, [event.target.name]: true})
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if(!Object.keys(errors).length) {
+            notify("you sign in succeussfully", "success")
+            console.log(data)
+        } else {
+            setTouched({
+                name: true,
+                email: true,
+                password: true,
+                confirmPassword: true,
+                isAccepted: true,
+            })
+            notify("invaild data")
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <h2>Sign up</h2>
                 <div>
                     <label>Name</label>
-                    <input type="text" name="name" value={data.name} onChange={changeHandler} />
+                    <input type="text" name="name" value={data.name} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="email" name="email" value={data.email} onChange={changeHandler} />
+                    <input type="email" name="email" value={data.email} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.email && touched.email && <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" value={data.password} onChange={changeHandler} />
+                    <input type="password" name="password" value={data.password} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.password && touched.password && <span>{errors.password}</span>}
                 </div>
                 <div>
                     <label>Confirm Password</label>
-                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} />
+                    <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
                 </div>
                 <div>
                     <label>i accepted terms of privacy policy</label>
-                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} />
+                    <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler} />
+                    {errors.isAccepted && touched.isAccepted && <span>{errors.isAccepted}</span>}
                 </div>
                 <div>
                     <a href='#'>Login</a>
                     <button type="submit">Sign Up</button>
                 </div>
             </form>
-            {console.log(data)}
+            <ToastContainer />
         </div>
     );
 };
